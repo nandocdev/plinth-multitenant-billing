c885bea7d21bb35a259d615638757a6b13bfa512
Fernando Castillo <fdocst@gmail.com>
2026-05-15 22:51:56 -0500

    feat(core): configuracion y actualizacion de gitignore

---
+++ src/Core/Config/dlocal.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'environment' => env('DLOCAL_ENV', 'sandbox'),
+    'login' => env('DLOCAL_LOGIN', ''),
+    'trans_key' => env('DLOCAL_TRANS_KEY', ''),
+    'secret_key' => env('DLOCAL_SECRET_KEY', ''),
+    'webhook_secret' => env('DLOCAL_WEBHOOK_SECRET', ''),
+];
